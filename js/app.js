d6b4f2462dbb999efb1558a2d7dd9f9868a8d648
Wes Bubeck <wesbubeck20@gmail.com>
2017-10-09 17:20:03 -0400

    building side nac

--- js/app.js
+++ js/app.js
@@ -13,3 +13,8 @@
       $('.carousel').carousel();
 
     });
+
+      // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
