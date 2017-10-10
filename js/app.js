9704d011baa470b6820e58b99d56937197a89565
Wes Bubeck <wesbubeck20@gmail.com>
2017-10-09 23:56:20 -0400

    added scroll spy

--- js/app.js
+++ js/app.js
@@ -18,3 +18,8 @@
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+
+// scrollspy js
+    $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
