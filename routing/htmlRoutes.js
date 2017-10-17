6222c04c5b2c43d9aecf6d925349dad92913a244
Wes Bubeck <wesbubeck20@gmail.com>
2017-10-17 13:17:53 -0400

    added server so that I can pus to heroku

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+// Dependencies
+// =============================================================
+var path = require("path");
+var express = require("express");
+
+// Routes
+// =============================================================
+
+//htmlRoutes.jss exports the following routes to servver
+module.exports = function(app) {
+    //if express can't find any static files, it will look for them in public
+    app.use(express.static("public"));
+    //the route for the home page
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
