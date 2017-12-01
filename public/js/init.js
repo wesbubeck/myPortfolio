fcdcd504de47cc13e1a01d6778f13157c1fe0e60
Wes Bubeck <wesbubeck20@gmail.com>
2017-11-30 22:26:33 -0500

    added the modal jquery code

--- public/js/init.js
+++ public/js/init.js
@@ -6,3 +6,11 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+//Modal init
+
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
