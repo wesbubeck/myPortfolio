53b88f7e0302cf6162a4fd4f74319e69efbc5bb9
Wes Bubeck <wesbubeck20@gmail.com>
2018-01-02 23:09:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var port = process.env.PORT || 3000;
+// // Sets up the Express App
+// // =============================================================
+// var app = express();
+// var port = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// ================================================================================
-// ROUTER
-// The below points server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+// // ================================================================================
+// // ROUTER
+// // The below points server to a series of "route" files.
+// // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// // ================================================================================
 
 
-require("./routing/htmlRoutes.js")(app);
+// require("./routing/htmlRoutes.js")(app);
 
-// Starts the server to begin listening
-// =============================================================
-app.listen(port, function() {
-    console.log("App listening on PORT " + port);
-});
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(port, function() {
+//     console.log("App listening on PORT " + port);
+// });
